6d7af9d46e0324bf8325d4747fb4f249ef6661c4
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-05-13 08:38:52 -0300

    properties

---
+++ Projetos/ExercicioComH2/src/main/java/com/example/ExercicioComH2/ExercicioComH2Application.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioComH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioComH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioComH2Application.class, args);
+	}
+
+}
---
+++ Projetos/ExercicioComH2/src/test/java/com/example/ExercicioComH2/ExercicioComH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioComH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioComH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

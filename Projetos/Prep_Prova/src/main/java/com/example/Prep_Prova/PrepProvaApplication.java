8021d615973cd77d1b4092a950bb1a8ac13749cc
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-05-27 10:16:42 -0300

    preparacao p/ prova

---
+++ Projetos/Prep_Prova/src/main/java/com/example/Prep_Prova/PrepProvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prep_Prova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepProvaApplication.class, args);
+	}
+
+}
---
+++ Projetos/Prep_Prova/src/test/java/com/example/Prep_Prova/PrepProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prep_Prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

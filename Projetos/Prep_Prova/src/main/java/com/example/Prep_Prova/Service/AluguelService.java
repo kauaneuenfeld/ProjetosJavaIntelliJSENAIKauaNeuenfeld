4d94d40ec1b09f7c23a74706dffd47a1f2050ffd
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-01 09:08:40 -0300

    preparacao p/ prova

--- Projetos/Prep_Prova/src/main/java/com/example/Prep_Prova/Service/AluguelService.java
+++ Projetos/Prep_Prova/src/main/java/com/example/Prep_Prova/Service/AluguelService.java
@@ -3,6 +3,7 @@ package com.example.Prep_Prova.Service;
 import com.example.Prep_Prova.Model.Aluguel;
 import com.example.Prep_Prova.Model.Jogo;
 import com.example.Prep_Prova.Repository.AluguelRepository;
+import com.example.Prep_Prova.Repository.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +14,27 @@ public class AluguelService {
 
     @Autowired
     private AluguelRepository aluguelRepository;
+    @Autowired
+    private JogoRepository jogoRepository;
 
     public Aluguel salvar(Aluguel aluguel){
-        return aluguelRepository.save(aluguel);
+       Long idJogo = aluguel.getJogo().getId();
+
+       Jogo jogoEncontrado = jogoRepository.findById(idJogo)
+               .orElseThrow(() -> new RuntimeException("Jogo não encontrado"));
+
+       if (aluguel.getQuantidade() > jogoEncontrado.getQuantidade()){
+           throw new RuntimeException("Quantidade indisponivel para alugar");
+       }
+       jogoEncontrado.setQuantidade(jogoEncontrado.getQuantidade() - aluguel.getQuantidade());
+
+       aluguel.setJogo(jogoEncontrado);
+
+
+       return aluguelRepository.save(aluguel);
     }
 
-    public List<Aluguel> listar(){
+    public  List<Aluguel> listar(){
         return aluguelRepository.findAll();
-    }
-
-    public Aluguel buscarPorid(Long id){
-        return aluguelRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Aluguel não encontrado"));
     }
 }
